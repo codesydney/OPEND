1de90a0733e455b51f9f2f12b379feb97986a619
Engramar <engramar@Engramars-Air.gateway>
2017-11-02 23:53:19 +1100

    fix db_create.py

--- db_create.py
+++ db_create.py
@@ -2,13 +2,16 @@ from app import db
 from models import nsw_birth_rates
 
 #create the database and the db tables
-db.create_all(nsw_birth_rates("2014","Albion Park NSW 2527","Albion Park","NSW","2527","193"))
-db.create_all(nsw_birth_rates("2011","Annandale NSW 2038","Annandale","NSW","2038","187"))
-db.create_all(nsw_birth_rates("2016","Armidale NSW 2350","Armidale","NSW","2350","266"))
-db.create_all(nsw_birth_rates("2001","Arncliffe NSW 2205","Arncliffe","NSW","2205","181"))
-db.create_all(nsw_birth_rates("2006","Artarmon NSW 2064","Artarmon","NSW","2064","181"))
-db.create_all(nsw_birth_rates("2016","Ashfield NSW 2131","Ashfield","NSW","2131","325"))
-db.create_all(nsw_birth_rates("2016","Auburn NSW 2144","Auburn","NSW","2144","751"))
+db.create_all()
+
+#create the database and the db tables
+db.session.add(nsw_birth_rates("2014","Albion Park NSW 2527","Albion Park","NSW","2527","193","1"))
+db.session.add(nsw_birth_rates("2011","Annandale NSW 2038","Annandale","NSW","2038","187","2"))
+db.session.add(nsw_birth_rates("2016","Armidale NSW 2350","Armidale","NSW","2350","266","3"))
+db.session.add(nsw_birth_rates("2001","Arncliffe NSW 2205","Arncliffe","NSW","2205","181","4"))
+db.session.add(nsw_birth_rates("2006","Artarmon NSW 2064","Artarmon","NSW","2064","181","5"))
+db.session.add(nsw_birth_rates("2016","Ashfield NSW 2131","Ashfield","NSW","2131","325","6"))
+db.session.add(nsw_birth_rates("2016","Auburn NSW 2144","Auburn","NSW","2144","751","7"))
 
 #commit the changes
 db.session.commit()
--- models.py
+++ models.py
@@ -1,6 +1,10 @@
 from app import db
 
 class nsw_birth_rates(db.Model):
+
+	__tablename__ = "nsw_birth_rates"
+	__table_args__ = {'extend_existing': True} 
+
 	year     = db.Column(db.Integer)
 	locality = db.Column(db.String(100))
 	suburb   = db.Column(db.String(100))
@@ -8,6 +12,15 @@ class nsw_birth_rates(db.Model):
 	postcode = db.Column(db.Integer)
 	count    = db.Column(db.Integer)
 	recid    = db.Column(db.Integer, primary_key = True)
+
+	def __init__(self, year, locality, suburb, state, postcode, count, recid):
+		self.year = year
+		self.locality = locality
+		self.suburb = suburb
+		self.state = state
+		self.postcode = postcode
+		self.count = count
+		self.recid = recid 
 
 	def __repr__(self):
 		return '{} {} {} {} {} {} {}'.format(self.year, self.locality, self.suburb, self.state, self.postcode, self.count, self.recid)
