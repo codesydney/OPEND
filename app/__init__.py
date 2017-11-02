5d6649ec86c04fd18fc49cd3fd1bbe9c55bc11c3
Engramar <engramar@Engramars-Air.gateway>
2017-11-02 22:52:02 +1100

    added create_db.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('config')
+#app.config.from_object('config')
+app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
 from app import views, models
