53f05692a389af5824f37fae5192c2f02fd5d1cb
Engramar <engramar@Engramars-Air.gateway>
2017-12-22 22:31:41 +1100

    Added address scaffold

--- db_create.py
+++ db_create.py
@@ -1,17 +1,17 @@
 from app import db
-from models import nsw_birth_rates
+from models import nsw_addresses
 
 #create the database and the db tables
 db.create_all()
 
 #create the database and the db tables
-db.session.add(nsw_birth_rates("2014","Albion Park NSW 2527","Albion Park","NSW","2527","193","1"))
-db.session.add(nsw_birth_rates("2011","Annandale NSW 2038","Annandale","NSW","2038","187","2"))
-db.session.add(nsw_birth_rates("2016","Armidale NSW 2350","Armidale","NSW","2350","266","3"))
-db.session.add(nsw_birth_rates("2001","Arncliffe NSW 2205","Arncliffe","NSW","2205","181","4"))
-db.session.add(nsw_birth_rates("2006","Artarmon NSW 2064","Artarmon","NSW","2064","181","5"))
-db.session.add(nsw_birth_rates("2016","Ashfield NSW 2131","Ashfield","NSW","2131","325","6"))
-db.session.add(nsw_birth_rates("2016","Auburn NSW 2144","Auburn","NSW","2144","751","7"))
+db.session.add(nsw_addresses("2014","1 George St, Albion Park NSW 2527","Albion Park","NSW","2527","193","1"))
+db.session.add(nsw_addresses("2011","1 George St, Annandale NSW 2038","Annandale","NSW","2038","187","2"))
+db.session.add(nsw_addresses("2016","1 George St, Armidale NSW 2350","Armidale","NSW","2350","266","3"))
+db.session.add(nsw_addresses("2001","1 George St, Arncliffe NSW 2205","Arncliffe","NSW","2205","181","4"))
+db.session.add(nsw_addresses("2006","1 George St, Artarmon NSW 2064","Artarmon","NSW","2064","181","5"))
+db.session.add(nsw_addresses("2016","1 George St, Ashfield NSW 2131","Ashfield","NSW","2131","325","6"))
+db.session.add(nsw_addresses("2016","1 George St, Auburn NSW 2144","Auburn","NSW","2144","751","7"))
 
 #commit the changes
 db.session.commit()
--- models.py
+++ models.py
@@ -1,21 +1,21 @@
 from app import db
 
-class nsw_birth_rates(db.Model):
+class nsw_addresses(db.Model):
 
-	__tablename__ = "nsw_birth_rates"
+	__tablename__ = "nsw_addresses"
 	__table_args__ = {'extend_existing': True} 
 
 	year     = db.Column(db.Integer)
-	locality = db.Column(db.String(100))
+	address  = db.Column(db.String(100))
 	suburb   = db.Column(db.String(100))
 	state    = db.Column(db.String(100))
 	postcode = db.Column(db.Integer)
 	count    = db.Column(db.Integer)
 	recid    = db.Column(db.Integer, primary_key = True)
 
-	def __init__(self, year, locality, suburb, state, postcode, count, recid):
+	def __init__(self, year, address, suburb, state, postcode, count, recid):
 		self.year = year
-		self.locality = locality
+		self.address = address
 		self.suburb = suburb
 		self.state = state
 		self.postcode = postcode
@@ -23,4 +23,4 @@ class nsw_birth_rates(db.Model):
 		self.recid = recid 
 
 	def __repr__(self):
-		return '{} {} {} {} {} {} {}'.format(self.year, self.locality, self.suburb, self.state, self.postcode, self.count, self.recid)
+		return '{} {} {} {} {} {} {}'.format(self.year, self.address, self.suburb, self.state, self.postcode, self.count, self.recid)
